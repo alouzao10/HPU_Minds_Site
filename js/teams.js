498b00cf4d9b030e35b0bcd89e280e7691b30f59
Tenocci <sekelj13@highpoint.edu>
2017-02-25 10:17:36 -0500

    changed tabs location and made it so they get active when clicked

--- js/teams.js
+++ js/teams.js
@@ -1,4 +1,12 @@
 function togglePane(x) {
-   var pane=document.getElementById(x);
-   
+   //take care of active nav-tab
+   $('.in-page-nav').removeClass('active');
+   var tab="#nav-tab-"+x;
+   $(tab).addClass('active');
+
+   //take care of visible team explanation
+   $('.tab-pane').removeClass('in active');
+   var pane='#'+x;
+   console.log(pane);
+   $(pane).addClass('in active');
 }
